14032f69b6490a62629c18175a27d0da5ea60716
xtalo <117736097+santiagoieshna@users.noreply.github.com>
2024-01-03 22:11:10 +0100

    components Done

--- bases-angular/src/app/counter/counter.component.ts
+++ bases-angular/src/app/counter/counter.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'selector-counter',
-    templateUrl: './counter.component.html'
+    selector: 'app-counter',
+    template: `
+            <h1>Hola</h1>
+            <button (click)="incrementar()">1</button>
+            <button (click)="reset()">Reset</button>
+            <button (click)="decrementar()">-1</button>
+            <h2>{{contador}}</h2>
+            `
 })
 
-export class counterComponent {
+export class CounterComponent {
     public contador : number = 0;
 
     incrementar():void{
         this.contador ++;
     }
-
     decrementar(): void{
         this.contador --;
     }
-
+    reset():void{
+        this.contador = 0;
+    }
 }
